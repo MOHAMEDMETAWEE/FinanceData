63e209c855fc515d0fe4421b27265541bfc2c14e
MOHAMED AHMED MOHAMED AHMED METAWEE <mohamedahmedmotawei@gmail.com>
2025-02-18 11:46:49 +0300

    Update Classification.py

--- Finance/Classification.py
+++ Finance/Classification.py
@@ -43,6 +43,7 @@ if uploaded_file is not None:
           pay1=cl1.selectbox('Choose Payment Type 1:',options=full['payment_type'].unique())
           pay2=cl2.selectbox('Choose Payment Type 2:',options=['UPI','Credit Card','Net Banking','Debit Card'])
           cl1.write(f'{full.query(f'{demo.lower().replace(' ',"_")}=="{cho}"').query(f'month=="{mon}"').query(f'payment_type=="{pay1}"')['spend'].count()}')
+          cl2.write(f'{full.query(f'{demo.lower().replace(' ',"_")}=="{cho}"').query(f'month=="{mon}"').query(f'payment_type=="{pay2}"')['spend'].count()}')
           f,a= plt.subplots(2)
           sns.histplot(full.query(f'{demo.lower().replace(' ',"_")}=="{cho}"').query(f'month=="{mon}"').query(f'payment_type=="{pay1}"')['spend'],
                        bins=250,
